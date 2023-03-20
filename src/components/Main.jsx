0304449eef4c20e28d15da5fa3d1803c0d1b9f9d
Angel Reyna <rugile@protonmail.com>
2023-03-20 18:51:52 +0200

    njumz

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,33 @@
 import { useState, useEffect } from 'react';
 import SearchMovie from './SearchMovie';
 import OneMovie from './oneMovie/OneMovie';
+import Modal from './Modal';
 
 const Main = () => {
     const [moviesFromApi, setMoviesFromApi] = useState([]);
-
-    // const getMovies = () => {
-    //     movieService().then((res) => {
-    //         setMoviesFromApi(res);
-    //     })
-    // }
-
-    // useEffect(() => {
-    //     getMovies();
-    // }, []);
-
-    // console.log(movies);
-
+    const [modalOpen, setOpenModal] = useState(false);
 
     const setResults = (movies) => {
         setMoviesFromApi(movies);
     }
-    console.log(moviesFromApi);
-    return (
-        <div>
+
+        return (
+        <div className='Main'>
             <SearchMovie setResults={setResults}/>
-            <OneMovie moviesFromApi={moviesFromApi}/>
+            <OneMovie moviesFromApi={moviesFromApi} />
+
+            <h1>Hey, Click on the modal</h1>
+            <button className='openModalBtn' onClick={() => {
+                    setOpenModal(true);
+                }}
+            >
+                Open
+            </button>
+                {/* if openModal is equal to true then render the Modal component  */}
+            {modalOpen && <Modal setOpenModal={setOpenModal} />} 
+
         </div>
-    )
+    );
 }
 
 export default Main
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
 
-const Modal = ({chosen}) => {
-    console.log(chosen.Title)
-    return (<div className="modal">
-            <h1>{chosen.Title} ({chosen.Year})</h1>
-            <div className="container-one">
-                <img src={chosen.Poster} alt={chosen.Title}/>
-                <div className="text">
-                    <p><strong>Plot: </strong>{chosen.Plot}</p>
-                    <p><strong>Genre: </strong>{chosen.Genre}</p>
-                    <p><strong>Actors: </strong>{chosen.Actors}</p>
-                    <p><strong>Writer: </strong>{chosen.Writer}</p>
-                    <p><strong>Runtime: </strong>{chosen.Runtime}</p>
-                    <p><strong>Released: </strong>{chosen.Released}</p>
-                    <p><strong>Production: </strong>{chosen.Production}</p>
+const Modal = ({setOpenModal}) => {
+
+    // const { Title, Year, imdbRating, Plot, Poster, Genre, Country } = openedMovie
+    
+  return (
+    <div className="modalBackground">
+        <div className="modalContainer">
+            <div className='titleCloseBtn'>
+            <button onClick={() => setOpenModal(false)}> X </button>
+            </div>
+
+            <div className='title'><h1>Are you cute?</h1>
                 </div>
+        
+            <div className='body'>
+                <p>
+                    The sexy people unite
+                </p>
+            </div>
+            <div className='footer'>
+                <button onClick={() => setOpenModal(false)}>Find other</button>
+                <button>Watch it</button>
             </div>
         </div>
-    )
+        Modal
+    </div>
+  )
 }
-export default Modal;
+
+export default Modal
+
+
+
+
+// const Modal = ({chosen}) => {
+//     console.log(chosen.Title)
+//     return (<div className="modal">
+//             <h1>{chosen.Title} ({chosen.Year})</h1>
+//             <div className="container-one">
+//                 <img src={chosen.Poster} alt={chosen.Title}/>
+//                 <div className="text">
+//                     <p><strong>Plot: </strong>{chosen.Plot}</p>
+//                     <p><strong>Genre: </strong>{chosen.Genre}</p>
+//                     <p><strong>Actors: </strong>{chosen.Actors}</p>
+//                     <p><strong>Writer: </strong>{chosen.Writer}</p>
+//                     <p><strong>Runtime: </strong>{chosen.Runtime}</p>
+//                     <p><strong>Released: </strong>{chosen.Released}</p>
+//                     <p><strong>Production: </strong>{chosen.Production}</p>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+// export default Modal;
